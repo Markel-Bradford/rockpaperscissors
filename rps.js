54d3328fef9254f609bf3bd449145601cf4755a1
markel-bradford <markel.bradford94@gmail.com>
2024-05-24 17:26:14 -0500

    Created playGame function. Added arguments for playRound and loop based on turns played.

--- rps.js
+++ rps.js
@@ -6,23 +6,65 @@ function getComputerChoice() {
     return choices[(Math.floor(Math.random() * choices.length))]
 }
 
-console.log(getComputerChoice())
-
 function getHumanChoice() {
     choice = prompt("Please enter rock, paper, or scissors: ")
+    choice = choice.toLowerCase();
     return choice
 }
 
-console.log(getHumanChoice())
-
-humanScore = 0
-computerScore = 0
-
-function playRound(humanChoice, computerChoice) {
-    
-}
 
 const humanSelection = getHumanChoice();    
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection)
+
+function playGame() {
+    let numTurns = 5
+    let turnsPlayed = 0
+    let humanScore = 0
+    let computerScore = 0
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock" && computerChoice === "scissors") {
+            ++humanScore
+            console.log("Human wins! Rock beats scissors")
+        } else if (humanChoice === "paper" && computerChoice === "rock"){
+            ++humanScore
+            console.log("Human wins! Paper beats rock")
+        } else if (humanChoice === "scissors" && computerChoice === "paper"){
+            ++humanScore
+            console.log("Human wins! Scissors beat paper")
+        } else if (computerChoice === "paper" && humanChoice === "rock"){
+            ++computerScore
+            console.log("Computer wins! Paper beats rock")
+        } else if (computerChoice === "scissors" && humanChoice === "paper"){
+            ++computerScore
+            console.log("Computer wins! Scissors beats paper")
+        } else if (computerChoice === "rock" && humanChoice === "scissors"){
+            ++computerScore
+            console.log("Computer wins! Rock beats scissors")
+        } else {
+            console.log("Draw! Play again");
+        };
+        console.log("Player score: " + humanScore)
+        console.log("Computer score: " + computerScore)
+    }
+
+    while (turnsPlayed < numTurns) {
+        ++turnsPlayed
+        playRound(getHumanChoice(), getComputerChoice())
+        console.log("Turns played: " + turnsPlayed)
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Congrats! You defeated the computer!")
+    } else if (computerScore > humanScore) {
+        console.log("Computer wins! Try your luck again?")
+    } else {
+        console.log("You tied! Play again?")
+    }
+
+    playRound()
+
+}
+
+playGame()
