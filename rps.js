2a55f46504e39b9d196a2f96a961db35546e00e2
markel-bradford <markel.bradford94@gmail.com>
2024-05-24 11:55:30 -0500

    create getHumanChoice function. Initialized score to for human and computer. Began playRound setup.

--- rps.js
+++ rps.js
@@ -2,12 +2,27 @@ function getComputerChoice() {
     // Create an array that lists all choices for the game
     choices = ["rock", "paper", "scissors"]
     {/* Use Math.floor to round down the float that Math.random returns and 
-return an actual integer that will match the choices index of 0, 1, or 2 */}
-    console.log(choices[(Math.floor(Math.random() * choices.length))])
+    return an actual integer that will match the choices index of 0, 1, or 2 */}
+    return choices[(Math.floor(Math.random() * choices.length))]
 }
 
-getComputerChoice()
+console.log(getComputerChoice())
 
 function getHumanChoice() {
-
+    choice = prompt("Please enter rock, paper, or scissors: ")
+    return choice
 }
+
+console.log(getHumanChoice())
+
+humanScore = 0
+computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    
+}
+
+const humanSelection = getHumanChoice();    
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
